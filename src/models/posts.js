64e5df20b0ade7195d20327ef5c0c6a568554339
CHUCHUDACHUCHU <yoon7548@gmail.com>
2022-12-23 20:59:52 +0900

    :card_file_box:fix: db models name 수정

--- src/models/posts.js
+++ src/models/posts.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            this.belongsTo(models.User, {
+            this.belongsTo(models.Users, {
                 foreignKey: 'userId',
             });
             this.hasMany(models.Comments, {
