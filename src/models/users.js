1ada5dc39618e4ead2d151537507d85c3fd6763d
CHUCHUDACHUCHU <yoon7548@gmail.com>
2022-12-23 21:37:07 +0900

    :sparkles:feat: 회원가입 기능 API

--- src/models/users.js
+++ src/models/users.js
@@ -47,7 +47,7 @@ module.exports = (sequelize, DataTypes) => {
             },
             token: {
                 type: DataTypes.STRING,
-                allowNull: false,
+                allowNull: true,
                 unique: true,
             },
             createdAt: {
